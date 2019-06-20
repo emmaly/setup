e4298a5dad33e11ab5791841f7c9f592bb05e10a
Emmaly Wilson <emmaly@emma.ly>
2019-06-20 10:06:46 -0700

    Update setup.sh to fix a lack of escaping newlines

--- setup.sh
+++ setup.sh
@@ -60,9 +60,9 @@ mkdir -p ~/.local/share/konsole ~/.config
 [ ! -f ~/.config/konsolerc ] && cp konsolerc -o ~/.config/konsolerc
 
 # Google Chrome install
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-	&& sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-	&& sudo apt-get update
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - \
+	&& sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+	&& sudo apt-get update \
 	&& sudo apt-get install -y google-chrome-stable --no-install-recommends
 
 # Google Cloud SDK install
