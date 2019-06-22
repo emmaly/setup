b91e5d186821b9732d4fdd10476eb84ac66a4243
Emmaly Wilson <emmaly@emma.ly>
2019-06-22 09:01:04 -0700

    ~/.yarn/bin to PATH

--- setup.sh
+++ setup.sh
@@ -190,6 +190,8 @@ else
 		&& sudo sh -c 'echo "deb https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list' \
 		&& sudo apt-get update \
 		&& sudo apt-get install -y yarn --no-install-recommends
+	grep -q "~/.yarn/bin" ~/.bashrc || \
+		echo 'PATH=$PATH:~/.yarn/bin' >> ~/.bashrc
 	yarn global add bower
 	yarn global add localtunnel
 	yarn global add polymer-cli
