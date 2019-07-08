464da6763c607ad24f258eae3a24800a454b4862
Emmaly Wilson <emmaly@emma.ly>
2019-07-08 00:38:14 -0700

    Added signal install

--- setup.sh
+++ setup.sh
@@ -282,3 +282,8 @@ NdCFTW7wY0Fb1fWJ+/KTsC4=
 	[ "$WRITE_SOURCE" -eq "1" ] && echo -e "### THIS FILE IS AUTOMATICALLY CONFIGURED ###\n# You may comment out this entry, but any other modifications may be lost.\ndeb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" | sudo tee $CODE_SOURCE_PART
 	sudo apt update && sudo apt install -y --no-install-recommends code
 fi
+
+# Install Signal
+curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal.list
+sudo apt update && sudo apt install signal-desktop
