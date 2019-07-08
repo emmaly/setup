30947a6e1c00931cf794efd46c087de88a71696f
Emmaly Wilson <emmaly@emma.ly>
2019-07-08 00:41:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -287,4 +287,4 @@ fi
 echo -e "\n[SIGNAL]"
 curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal.list
-sudo apt update && sudo apt install signal-desktop
+sudo apt update && sudo apt install -y --no-install-recommends signal-desktop
