8b4f26225715e1e120faba9d8785b5aaade113e8
Emmaly Wilson <emmaly@emma.ly>
2019-07-22 12:11:45 -0700

    Run update-authorized-keys when running peer-setup

--- peer-setup.sh
+++ peer-setup.sh
@@ -13,6 +13,7 @@ fi
 if which crontab >/dev/null; then
 	if [ -d "$DIR" -a -f "$DIR/update-authorized-keys.sh" ]; then
 		(crontab -l | grep -v "update-authorized-keys.sh"; echo "0 * * * * $DIR/update-authorized-keys.sh") | crontab -
+		$DIR/update-authorized-keys.sh
 	else
 		(crontab -l | grep -v "update-authorized-keys.sh") | crontab -
 	fi
