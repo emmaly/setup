d7a5277908512b46917fbb39bb149b0515dbc321
Emmaly Wilson <emmaly@emma.ly>
2019-07-22 11:54:58 -0700

    Add sync of authorized_keys

---
+++ peer-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=$HOME/.emmaly-setup
+
+if which git >/dev/null; then
+	if [ ! -d "$DIR" ]; then
+		git clone https://github.com/emmaly/setup.git "$DIR"
+	else
+		git -C "$DIR" pull
+	fi
+fi
+
+if which crontab >/dev/null; then
+	if [ -d "$DIR" -a -f "$DIR/update-authorized-keys.sh" ]; then
+		(crontab -l | grep -v "update-authorized-keys.sh"; echo "0 * * * * $DIR/update-authorized-keys.sh") | crontab -
+	else
+		(crontab -l | grep -v "update-authorized-keys.sh") | crontab -
+	fi
+fi
