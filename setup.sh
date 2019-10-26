8f21d64a62f6d678fcf581d2f1f5ed0763bf3f3b
Emmaly <emmaly@emma.ly>
2019-10-25 19:27:50 -0700

    Removed Signal

--- setup.sh
+++ setup.sh
@@ -341,14 +341,14 @@ NdCFTW7wY0Fb1fWJ+/KTsC4=
 fi
 
 # Install Signal
-echo -e "\n[SIGNAL]"
-if which signal-desktop >/dev/null; then
-	echo "Signal already installed, skipping."
-else
-	curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
-	echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal.list
-	sudo apt update && sudo apt install -y --no-install-recommends signal-desktop
-fi
+#echo -e "\n[SIGNAL]"
+#if which signal-desktop >/dev/null; then
+#	echo "Signal already installed, skipping."
+#else
+#	curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+#	echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee /etc/apt/sources.list.d/signal.list
+#	sudo apt update && sudo apt install -y --no-install-recommends signal-desktop
+#fi
 
 # Install Keybase
 echo -e "\n[KEYBASE]"
