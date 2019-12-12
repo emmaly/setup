e95381d1658930cddfc3f9c49649eb4fb25a325e
Emmaly <emmaly@emma.ly>
2019-12-11 19:21:13 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,10 +20,6 @@ mkdir -vp ~/code ~/.local/share ~/.config ~/.yarn/bin
 # Packages
 echo -e "\n[PACKAGES]"
 
-# Add unstable (for access to wireguard)
-echo "deb http://deb.debian.org/debian/ unstable main" | sudo tee /etc/apt/sources.list.d/unstable.list
-echo -e "Package: *\nPin: release a=unstable\nPin-Priority: 90" | sudo tee /etc/apt/preferences.d/limit-unstable
-
 ## General Updates
 echo -e "\nUpdating package cache..."
 sudo apt-get update
