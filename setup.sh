d612c7d7b068dcd177f0970e9be509d6c85c8822
Emmaly <emmaly@emma.ly>
2020-04-02 00:32:41 -0700

    Update repo for Google Cloud SDK

--- setup.sh
+++ setup.sh
@@ -135,7 +135,7 @@ fi
 echo -e "\n[GOOGLE CLOUD SDK]"
 if [ ! -f /etc/apt/sources.list.d/google-cloud-sdk.list ]; then
 	curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
-	echo "deb http://packages.cloud.google.com/apt cloud-sdk-$(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list >/dev/null && \
+	echo "deb http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list >/dev/null && \
 	sudo apt-get update
 fi
 dpkg -l google-cloud-sdk 2>/dev/null | grep ^ii >/dev/null || sudo apt-get install --no-install-recommends -y google-cloud-sdk
