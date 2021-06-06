5997ab0713ba58a5d85947c60d16657c91271bee
Emmaly <emmaly@emma.ly>
2021-06-06 16:54:52 -0700

    Workaround wslusc/WSL2 path length issue

--- x-wsl2.sh
+++ x-wsl2.sh
@@ -11,10 +11,18 @@ if [ ! -z "$IS_WSL2" ]; then
 
 	[ ! -f "$HOME/wsl.sh" ] && cp wsl.sh "$HOME/wsl.sh" && chmod 755 "$HOME/wsl.sh"
 
-	if which wslusc >/dev/null && [ -x "$HOME/wsl.sh" ]; then
-		SHORTCUT_NAME="$(grep -oP '^\s*#\s*NAME=\K.*' "$HOME/wsl.sh")"
+	# if which wslusc >/dev/null && [ -x "$HOME/wsl.sh" ]; then
+	# 	SHORTCUT_NAME="$(grep -oP '^\s*#\s*NAME=\K.*' "$HOME/wsl.sh")"
+	# 	SHORTCUT_NAME="${SHORTCUT_NAME:-WSL}"
+	# 	wslusc -n "$SHORTCUT_NAME" -g "$HOME/wsl.sh"
+	# fi
+
+	# WORKAROUND an issue in Ubuntu/WSL causing wslusc path length issue
+	[ -f "$HOME/wsl.sh" ] && [ ! -L "/bin/_emwsl.sh" ] && sudo ln -s "$HOME/wsl.sh" "/bin/_emwsl.sh"
+	if which wslusc >/dev/null && [ -x "/bin/_emwsl.sh" ]; then
+		SHORTCUT_NAME="$(grep -oP '^\s*#\s*NAME=\K.*' "/bin/_emwsl.sh")"
 		SHORTCUT_NAME="${SHORTCUT_NAME:-WSL}"
-		wslusc -n "$SHORTCUT_NAME" -g "$HOME/wsl.sh"
+		wslusc -n "$SHORTCUT_NAME" -g "/bin/_emwsl.sh"
 	fi
 
 	[ -x "$(which docker)" ] && echo "Want Docker?  You'll need https://hub.docker.com/editions/community/docker-ce-desktop-windows/ in Windows."
